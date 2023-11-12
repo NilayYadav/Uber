baf18498d9baed8a4b44e4d677d4452ec801bfe3
NilayYadav <nilayyadav10@gmail.com>
2023-11-12 16:16:55 +0530

    Displaying Map/ Manage Location

--- Uber/Managers/LocationManager.swift
+++ Uber/Managers/LocationManager.swift
@@ -5,4 +5,25 @@
 //  Created by Nilay on 12/11/23.
 //
 
-import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
+    }
+}

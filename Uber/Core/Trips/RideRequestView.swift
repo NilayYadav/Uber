219eb5643172b3091d042817b6fc4944b1f8cf3d
NilayYadav <nilayyadav10@gmail.com>
2023-11-16 20:10:54 +0530

    RideRequestView

--- Uber/Core/Trips/RideRequestView.swift
+++ Uber/Core/Trips/RideRequestView.swift
@@ -9,7 +9,129 @@ import SwiftUI
 
 struct RideRequestView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Capsule()
+                .foregroundColor(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 8, height: 8)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 32)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 8, height: 8)
+                }
+                
+                VStack(alignment: .leading, spacing: 18) {
+                    HStack {
+                        Text("Current Location")
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(.gray)
+                        
+                        Spacer()
+                        
+                        Text("10:00 AM")
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundColor(.gray)
+                        
+                    }
+                    .padding(.bottom, 10)
+                    
+                    HStack {
+                        Text("Brooklyn")
+                            .font(.system(size: 16, weight: .semibold))
+                        
+                        Spacer()
+                        
+                        Text("10:15 AM")
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(.leading, 8)
+            }
+            .padding()
+            
+            Divider()
+            
+            Text("SUGGESTED RIDES")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .padding()
+                .foregroundColor(.gray)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 10) {
+                    ForEach(0 ..< 3, id: \.self) { _ in
+                        VStack(alignment: .leading) {
+                            Image("uber-x")
+                                .resizable()
+                                .scaledToFit()
+                            
+                            VStack(spacing: 4) {
+                                Text("Uber X")
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(.gray)
+                                
+                                Text("22.5$")
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(.gray)
+                            }
+                            .padding(8)
+                            
+                        }
+                        .frame(width: 112, height: 140)
+                        .background(Color(.systemGroupedBackground))
+                        .cornerRadius(10)
+                    }
+                }
+            }
+            .padding(.horizontal)
+            
+            Divider()
+                .padding(.vertical, 8)
+            
+            HStack(spacing: 12) {
+                Text("VISA")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(6)
+                    .background(.blue)
+                    .cornerRadius(4)
+                    .foregroundStyle(.white)
+                    .padding(.leading)
+                
+                Text("XXXXXXX12345")
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .imageScale(.medium)
+                    .padding()
+            }
+            .frame(height: 50)
+            .background(Color(.systemGroupedBackground))
+            .cornerRadius(10)
+            .padding(.horizontal)
+            
+            Button(action: {}, label: {
+                Text("Comfirm Ride")
+                    .fontWeight(.bold)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+            })
+        }
+        .background(.white)
     }
 }
 

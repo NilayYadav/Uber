4c4cc659cdbb6301c954c930fbceedbed003c560
NilayYadav <nilayyadav10@gmail.com>
2023-11-16 20:10:53 +0530

    RideRequestView

---
+++ Uber/Core/Trips/RideRequestView.swift
@@ -0,0 +1,18 @@
+//
+//  RideRequestView.swift
+//  Uber
+//
+//  Created by Nilay on 16/11/23.
+//
+
+import SwiftUI
+
+struct RideRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RideRequestView()
+}
